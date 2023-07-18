f6e30ad71f3e7422da07dcb2fdcc4d351ba89d1c
veeramuthuk <veeramuthu.kathirvel@fssa.freshworks.com>
2023-07-18 13:46:43 +0530

    day06

---
+++ src/main/java/day06/Pratice/DuplicateTasks.java
@@ -0,0 +1,31 @@
+
+package day06.Pratice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DuplicateTasks {
+
+	public static void main(String[] args) {
+
+		List<String> cityList = new ArrayList<String>();
+		cityList.add("Tamilnadu");
+		cityList.add("kerala");
+		cityList.add("karnataka");
+		cityList.add("delhi");
+		cityList.add("Tamilnadu");
+
+		List<String> NewCityList = new ArrayList<String>();
+
+		for (String city : cityList) {
+			if (!NewCityList.contains(city)) {
+				NewCityList.add(city);
+			}
+		}
+
+		for (String city : NewCityList) {
+			System.out.println(city);
+		}
+	}
+
+}
