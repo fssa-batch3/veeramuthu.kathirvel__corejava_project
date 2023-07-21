93dec3765145078d7204abb580eb62e415b01350
veeramuthuk <veeramuthu.kathirvel@fssa.freshworks.com>
2023-07-21 12:25:29 +0530

    day10

---
+++ src/main/java/day10/Solved/ThrowRunTimeExceptionDemo.java
@@ -0,0 +1,26 @@
+package day10.Solved;
+
+public class ThrowRunTimeExceptionDemo {
+	
+	public static boolean validate(Task task)  {
+		if (task == null)
+			throw new IllegalArgumentException("Task is null");
+		else if (task.getName() == null || "".equals(task.getName()))
+			throw new IllegalArgumentException("Task name is empty");
+		return true;
+	}
+}
+
+/**
+ * Example for demonstrating Exception Hierarchy
+ * showing how RuntimeExceptions are handled.
+ */
+public class ThrowRunTimeExceptionDemo {
+	public static void main(String[] args) {
+		Task task = new Task();
+//		task.setName("Creating test case"); Commenting this so that exception is thrown
+		// Not surrounding with try catch as Illegal is a runtime exception
+		ThrowRunTimeExceptionDemo.validate(task);
+	}
+
+}
